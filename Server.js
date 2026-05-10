cafbd45b218209e185a8fd18ee7886bb6a788f75
lavish kamboj <lavishkamboj430@gmail.com>
2026-05-10 21:31:24 +0530

    Add files via upload

---
+++ Server.js
@@ -0,0 +1,5 @@
+const {server} = require("./app")
+server.listen("3000",()=>
+{
+    console.log("Server Running on Port 3000!")
+})
---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const cors = require("cors")
+const http = require("http")
+const { Server } = require("socket.io")
+const app = express()
+app.use(express.json())
+app.use(cors())
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"]
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log("User Connected:", socket.id)
+    socket.on("send-message", ({ Usermsg, time,inputValue }) => {
+        io.emit("recieve", { msg: Usermsg, time, senderId: socket.id ,inputValue:inputValue})
+    })
+    socket.on("disconnect", () => {
+
+        console.log("User left")
+
+    })
+})
+
+module.exports = { server }
